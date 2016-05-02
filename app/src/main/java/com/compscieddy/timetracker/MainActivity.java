e3383a126f5bf5d04ac99ee2819ccfd8175e4d34
Eddie Lee <eddieberklee@gmail.com>
2016-05-02 13:24:29 -0700

    New launcher icon, able to create Event objects

--- app/src/main/java/com/compscieddy/timetracker/MainActivity.java
+++ app/src/main/java/com/compscieddy/timetracker/MainActivity.java
@@ -3,32 +3,79 @@ package com.compscieddy.timetracker;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.compscieddy.eddie_utils.Etils;
 import com.compscieddy.eddie_utils.Lawg;
 import com.compscieddy.timetracker.models.Day;
+import com.compscieddy.timetracker.models.Event;
 
 import java.util.Calendar;
 import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
   private static final Lawg lawg = Lawg.newInstance(MainActivity.class.getSimpleName());
 
   Day mCurrentDay;
+  @Bind(R.id.new_event_input) EditText mNewEventInput;
+  @Bind(R.id.new_event_add_button) View mNewEventAddButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    LayoutInflater layoutInflater = getLayoutInflater();
+    ViewGroup rootView = (ViewGroup) layoutInflater.inflate(R.layout.activity_main, null);
+    setContentView(rootView);
+    ButterKnife.bind(this);
 
     mCurrentDay = getCurrentDay();
     if (mCurrentDay == null) {
       lawg.d("Current Day not found, creating a new one");
       mCurrentDay = createCurrentDay();
     }
+    initEvents();
 
+    View itemEventView = layoutInflater.inflate(R.layout.item_event_layout, rootView);
+
+    mNewEventInput.addTextChangedListener(mEventInputTextWatcher);
+    mNewEventAddButton.setOnClickListener(mAddButtonOnClickListener);
   }
+
+  TextWatcher mEventInputTextWatcher = new TextWatcher() {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+    @Override
+    public void afterTextChanged(Editable s) {
+      if (s.length() == 0) {
+        mNewEventAddButton.setVisibility(View.INVISIBLE);
+      } else {
+        mNewEventAddButton.setVisibility(View.VISIBLE);
+      }
+    }
+  };
+
+  View.OnClickListener mAddButtonOnClickListener = new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+      Event newEvent = new Event(mCurrentDay);
+      newEvent.setTitle(mNewEventInput.getText().toString());
+      newEvent.setColor(mNewEventInput.getCurrentTextColor());
+      mNewEventInput.setText("");
+    }
+  };
 
   @Nullable
   public Day getCurrentDay() {
@@ -56,6 +103,13 @@ public class MainActivity extends AppCompatActivity {
     int dayOfYear = calendar.get(Calendar.DAY_OF_YEAR);
     Day day = new Day(year, dayOfYear);
     return day;
+  }
+
+  /**
+   * mCurrentDay needs to be populated before this method.
+   */
+  private void initEvents() {
+
   }
 
 }
--- app/src/main/java/com/compscieddy/timetracker/models/Event.java
+++ app/src/main/java/com/compscieddy/timetracker/models/Event.java
@@ -8,12 +8,23 @@ import com.orm.SugarRecord;
 public class Event extends SugarRecord {
 
   String title;
+  int color;
   Day day;
 
   public Event() {}
 
   public Event(Day day) {
     this.day = day;
+    save();
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+    save();
+  }
+
+  public void setColor(int color) {
+    this.color = color;
     save();
   }
 
